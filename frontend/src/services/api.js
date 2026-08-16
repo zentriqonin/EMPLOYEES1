64fc1d0174fa7a14159efd627b5668d8f7d4309b
Arunkumar Poovarasu <Arunkumar2003arun@gmail.com>
2026-08-16 20:52:29 +0530

    chore: update configuration and api services

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://employees1-9aoh.onrender.com/api',
+  baseURL: '/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
